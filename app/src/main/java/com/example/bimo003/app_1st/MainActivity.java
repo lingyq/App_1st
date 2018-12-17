f298953c389ac372194dce500d9358bb1a7a498d
lingyq <lyq.roco@foxmail.com>
2018-12-17 14:11:26 +0800

    Init App_1st for AS3.2

---
+++ app/src/main/java/com/example/bimo003/app_1st/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bimo003.app_1st;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
